38e9fc2b364667e540e94f4843466fb1854a5f98
egoroff <egoroff@gmail.com>
2021-04-30 20:04:19 +0300

    duplicate values test. Output all in tests

--- src/lib.rs
+++ src/lib.rs
@@ -29,5 +29,36 @@ trim_trailing_whitespace = false"###;
         assert!(star.contains_key("charset"));
         let md = conf.section(Some("*.md")).unwrap();
         assert!(md.contains_key("max_line_length"));
+
+        for (sec, prop) in &conf {
+            println!("Section: {:?}", sec);
+            for (key, value) in prop.iter() {
+                println!("{:?}:{:?}", key, value);
+            }
+        }
+    }
+
+    #[test]
+    fn parse_with_duplicate_values() {
+        // Arrange
+        let config = r###"
+[*]
+charset = utf-8
+charset = utf-7
+"###;
+
+        // Act
+        let conf = Ini::load_from_str(config).unwrap();
+
+        // Assert
+        let star = conf.section(Some("*")).unwrap();
+        assert!(star.contains_key("charset"));
+
+        for (sec, prop) in &conf {
+            println!("Section: {:?}", sec);
+            for (key, value) in prop.iter() {
+                println!("{:?}:{:?}", key, value);
+            }
+        }
     }
 }
