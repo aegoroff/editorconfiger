b0529ac198f7dbf55fdce56bd09d745893642e40
egoroff <egoroff@gmail.com>
2021-05-01 08:03:10 +0300

    complex section name test added

--- src/lib.rs
+++ src/lib.rs
@@ -63,6 +63,29 @@ charset = utf-7
     }
 
     #[test]
+    fn parse_complex_section_name() {
+        // Arrange
+        let config = r###"
+[*.{js,jsx,ts,tsx,vue}]
+indent_style = space
+indent_size = 2
+trim_trailing_whitespace = true
+insert_final_newline = true
+"###;
+
+        // Act
+        let conf = Ini::load_from_str(config).unwrap();
+
+        // Assert
+        for (sec, prop) in &conf {
+            println!("Section: {:?}", sec);
+            for (key, value) in prop.iter() {
+                println!("{:?}:{:?}", key, value);
+            }
+        }
+    }
+
+    #[test]
     fn parse_invalid_syntax() {
         // Arrange
         let config = r###"
