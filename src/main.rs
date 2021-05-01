dce95d43849cd7bea0ca590761b385508ba8183f
egoroff <egoroff@gmail.com>
2021-05-01 23:06:14 +0300

    command line interface draft

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
+use clap::{App, Arg, ArgMatches, SubCommand};
 
+#[macro_use]
+extern crate clap;
+
+fn main() {
+    let app = build_cli();
+    let matches = app.get_matches();
+
+    match matches.subcommand() {
+        ("c", Some(cmd)) => compare(cmd),
+        ("v", Some(cmd)) => validate(cmd),
+        _ => {}
+    }
+}
+
+fn validate(cmd: &ArgMatches) {
+    // TODO: implement
+}
+
+fn compare(cmd: &ArgMatches) {
+    // TODO: implement
+}
+
+fn build_cli() -> App<'static, 'static> {
+    return App::new("editorconfiger")
+        .version(crate_version!())
+        .author("egoroff <egoroff@gmail.com>")
+        .about(".editorconfig files tool")
+        .subcommand(
+            SubCommand::with_name("v")
+                .aliases(&["validate"])
+                .about("Validate single .editorconfig file")
+                .arg(
+                    Arg::with_name("PATH")
+                        .help("Sets directory path to .editorconfig file")
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("c")
+                .aliases(&["compare"])
+                .about("Compare two .editorconfig files")
+                .arg(
+                    Arg::with_name("FILE1")
+                        .help("Sets directory path to the first .editorconfig file")
+                        .required(true)
+                        .index(1),
+                ).arg(
+                Arg::with_name("FILE2")
+                    .help("Sets directory path to the second .editorconfig file")
+                    .required(true)
+                    .index(2),
+            ),
+        );
 }
