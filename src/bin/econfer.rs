0fbd403c0e8e94dda3bf02f5b6728205e2be3075
egoroff <egoroff@gmail.com>
2021-05-07 08:33:01 +0300

    use crate_name! macro instead of hardcoded app name string

--- src/bin/econfer.rs
+++ src/bin/econfer.rs
@@ -47,7 +47,7 @@ fn compare(cmd: &ArgMatches) {
 }
 
 fn build_cli() -> App<'static, 'static> {
-    return App::new("econfer")
+    return App::new(crate_name!())
         .version(crate_version!())
         .author("egoroff <egoroff@gmail.com>")
         .about(".editorconfig files tool")
