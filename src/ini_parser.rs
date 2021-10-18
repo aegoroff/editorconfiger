4f4b3cbb075925b00d2d2cff54da386aeb2466a1
egoroff <egoroff@gmail.com>
2021-10-18 10:00:34 +0300

    feat: Comments support added to ini parser

--- src/ini_parser.rs
+++ src/ini_parser.rs
@@ -1,12 +1,13 @@
 use nom::bytes::complete::is_not;
 use nom::combinator::ParserIterator;
 use nom::error::VerboseError;
-use nom::sequence::{delimited, terminated};
+use nom::sequence;
 use nom::{character::complete, combinator, IResult, Parser};
 
 enum IniLine<'a> {
     Head(&'a str),
     Line(&'a str, &'a str),
+    Comment(&'a str),
 }
 
 fn parse_ini<'a>(input: &'a str) -> Vec<IniLine<'a>> {
@@ -35,7 +36,7 @@ where
 {
     combinator::iterator(
         input,
-        terminated(complete::not_line_ending, complete::line_ending),
+        sequence::terminated(complete::not_line_ending, complete::line_ending),
     )
 }
 
@@ -44,10 +45,36 @@ where
     E: nom::error::ParseError<&'a str> + std::fmt::Debug,
 {
     let result: IResult<&'a str, &'a str, E> = s_expr(is_not("]"))(input);
-    match result {
+    let head = match result {
         Ok((_trail, matched)) => Some(IniLine::Head(matched)),
         Err(_e) => None,
+    };
+
+    if let Some(IniLine::Head(_h)) = head {
+        return head;
     }
+
+    let result: IResult<&'a str, (&'a str, &'a str), E> = key_value(input);
+    let kv = match result {
+        Ok((_trail, (k, v))) => Some(IniLine::Line(k, v)),
+        Err(_e) => None,
+    };
+
+    if let Some(IniLine::Line(_k, _v)) = kv {
+        return kv;
+    }
+
+    let result: IResult<&'a str, &'a str, E> = comment(input);
+    let c = match result {
+        Ok((_trail, c)) => Some(IniLine::Comment(c)),
+        Err(_e) => None,
+    };
+
+    if let Some(IniLine::Comment(_c)) = c {
+        return c;
+    }
+
+    None
 }
 
 fn s_expr<'a, F, E>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, &'a str, E>
@@ -55,7 +82,23 @@ where
     F: Parser<&'a str, &'a str, E>,
     E: nom::error::ParseError<&'a str> + std::fmt::Debug,
 {
-    delimited(complete::char('['), inner, complete::char(']'))
+    sequence::delimited(complete::char('['), inner, complete::char(']'))
+}
+
+fn key_value<'a, E>(input: &'a str) -> IResult<&'a str, (&'a str, &'a str), E>
+where
+    E: nom::error::ParseError<&'a str> + std::fmt::Debug,
+{
+    let mut action = sequence::separated_pair(is_not("="), complete::char('='), is_not("="));
+    action(input)
+}
+
+fn comment<'a, E>(input: &'a str) -> IResult<&'a str, &'a str, E>
+where
+    E: nom::error::ParseError<&'a str> + std::fmt::Debug,
+{
+    let mut action = sequence::preceded(complete::char('#'), is_not("\n\r"));
+    action(input)
 }
 
 #[cfg(test)]
@@ -72,6 +115,27 @@ mod tests {
             ("[a]\n\n[b]", vec![IniLine::Head("a"), IniLine::Head("b")]),
             ("[a]", vec![IniLine::Head("a")]),
             ("[a]\r\n", vec![IniLine::Head("a")]),
+            (
+                "[a]\nk=v",
+                vec![IniLine::Head("a"), IniLine::Line("k", "v")],
+            ),
+            (
+                "[a]\nk=v\n[b]",
+                vec![
+                    IniLine::Head("a"),
+                    IniLine::Line("k", "v"),
+                    IniLine::Head("b"),
+                ],
+            ),
+            (
+                "[a]\n# test\nk=v\n[b]",
+                vec![
+                    IniLine::Head("a"),
+                    IniLine::Comment(" test"),
+                    IniLine::Line("k", "v"),
+                    IniLine::Head("b"),
+                ],
+            ),
         ];
 
         // Act & Assert
