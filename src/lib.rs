3e16a289acdba8ab72da09a582ad59e44a48646d
egoroff <egoroff@gmail.com>
2021-12-31 00:18:30 +0300

    refactor: is_ok method of ValidationResult made public

--- src/lib.rs
+++ src/lib.rs
@@ -89,7 +89,7 @@ impl<'input> ValidationResult<'input> {
         ValidationState::from(self)
     }
 
-    fn is_ok(&self) -> bool {
+    pub fn is_ok(&self) -> bool {
         self.duplicate_properties.is_empty()
             && self.duplicate_sections.is_empty()
             && self.similar_properties.is_empty()
